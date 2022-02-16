44b190eeee4face5bf631139769d7aeffc39d5d2
Cian Butler <ciandevelops@gmail.com>
2022-02-16 11:24:17 -0500

    structure set up

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World!</h1>
+
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+      <Hero />
     </div>
   );
 }
